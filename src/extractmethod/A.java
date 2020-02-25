a22c248aba208d1353dafd41d431897a66a4715d
Rodrigo Brito <rig.10@hotmail.com>
2020-02-25 20:14:51 -0300

    refactor: extracting method isEven from Class A

--- src/extractmethod/A.java
+++ src/extractmethod/A.java
@@ -2,10 +2,14 @@ package extractmethod;
 
 public class A {
 	public void M1(int v) {
-		if (v % 2 == 0) {
+		if (this.isEven(v)) {
 			System.out.println("even");
 		} else {
 			System.out.println("odd");
 		}
+	}
+
+	public boolean isEven(int v) {
+		return v % 2 == 0;
 	}
 }
