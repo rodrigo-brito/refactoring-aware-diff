7d22eaf1088d5a321dd50d2b2608dce6a1b2e3af
Rodrigo Brito <rig.10@hotmail.com>
2020-02-25 18:53:20 -0300

    initial version

---
+++ src/p3/A.java
@@ -0,0 +1,11 @@
+package p3;
+
+public class A {
+  public void m1() {
+    System.out.println("m1");
+  }
+
+  public void m2() {
+    System.out.println("m2");
+  }
+}
---
+++ src/p9/A.java
@@ -0,0 +1,8 @@
+package p9;
+
+public class A {
+  public void m5() {
+    int value = 5;
+    System.out.println("m" + value);
+  }
+}
