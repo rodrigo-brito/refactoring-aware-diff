ae01db07cbe06965733af11782c047a12fb877db
Rodrigo Brito <rig.10@hotmail.com>
2020-02-25 20:40:12 -0300

    refactor: pushing down method M2 from Class A to B

--- src/pushdownmethod/A.java
+++ src/pushdownmethod/A.java
@@ -4,8 +4,4 @@ public class A {
 	public void M1() {
 		System.out.println("M1");
 	}
-
-	public void M2() {
-		System.out.println("M2");
-	}
 }
--- src/pushdownmethod/B.java
+++ src/pushdownmethod/B.java
@@ -1,6 +1,10 @@
 package pushdownmethod;
 
 public class B extends A {
+	public void M2() {
+		System.out.println("M2");
+	}
+
 	public void M3() {
 		System.out.println("M3");
 	}
