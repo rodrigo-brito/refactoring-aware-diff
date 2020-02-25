1bc247ea729abd5fa2f031297460e920f145c667
Rodrigo Brito <rig.10@hotmail.com>
2020-02-25 20:33:30 -0300

    refactor: inlining method printNumberType into M1

--- src/inlinemethod/A.java
+++ src/inlinemethod/A.java
@@ -2,10 +2,6 @@ package inlinemethod;
 
 public class A {
 	public void M1(int v) {
-		this.printNumberType(v);
-	}
-
-	public void printNumberType(int v) {
 		if (v % 2 == 0) {
 			System.out.println("even");
 		} else {
