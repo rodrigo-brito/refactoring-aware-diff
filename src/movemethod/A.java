92f5f997a6f35814144a92e2a4576c1aa33522d8
Rodrigo Brito <rig.10@hotmail.com>
2020-02-25 18:55:08 -0300

    refactor: moving method M1 from Class A to B

--- src/movemethod/A.java
+++ src/movemethod/A.java
@@ -1,10 +1,6 @@
 package movemethod;
 
 public class A {
-	public void M1() {
-		System.out.println("M1");
-	}
-
 	public void M2() {
 		System.out.println("M2");
 	}
--- src/movemethod/B.java
+++ src/movemethod/B.java
@@ -1,6 +1,10 @@
 package movemethod;
 
 public class B {
+	public void M1() {
+		System.out.println("M1");
+	}
+
 	public void M3() {
 		System.out.println("M3");
 	}
